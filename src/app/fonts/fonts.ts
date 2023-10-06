4ccf202c2167649f75c0715895af45f6b3bd9ee6
ajea <smithajea@rocketmail.com>
2023-10-06 18:10:20 -0400

    style: setup fonts and images

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Source_Code_Pro } from 'next/font/google';
+
+const sourceCodePro300 = Source_Code_Pro({ weight: '300', subsets: ['latin'] });
+const inter400 = Inter({ weight: '400', subsets: ['latin'] });
+const inter600 = Inter({ weight: '600', subsets: ['latin'] });
+ 
+export { inter400, inter600, sourceCodePro300 };
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import { inter400 } from './fonts/fonts';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-      Hello world
-    </main>
-  );
+  return <main className={inter400.className}>hello world</main>
 }
