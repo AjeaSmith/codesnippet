e68e14a1d41975f931424a4fb6999c83429ddede
ajea <smithajea@rocketmail.com>
2023-11-12 23:18:01 -0500

    refactor: conditional render search input on certain pages

--- src/app/ui/search/Search.tsx
+++ src/app/ui/search/Search.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
@@ -11,8 +12,6 @@ const Search = () => {
 
   // set the query params with search term
   const handleSearch = useDebouncedCallback((term) => {
-    // console.log(`Searching... ${term}`);
-
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -25,16 +24,23 @@ const Search = () => {
 
   return (
     <header className="w-full border-b-2 border-black px-8 py-4 self-center">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center flex-1">
-          <MagnifyingGlassIcon className="w-6 text-[#F4FAFF] opacity-60" />
-          <input
-            className="w-full px-4 py-2 bg-transparent outline-none text-[#F4FAFF] placeholder:text-[#F4FAFF] placeholder:opacity-40"
-            type="text"
-            onChange={(e) => handleSearch(e.target.value)}
-            placeholder="Find by title, tag, code..."
-          />
-        </div>
+      <div
+        className={clsx('flex items-center', {
+          'justify-end': pathname.includes('/dashboard/code'),
+          'justify-between': !pathname.includes('/dashboard/code'),
+        })}
+      >
+        {!pathname.includes('/dashboard/code') && (
+          <div className="flex items-center flex-1">
+            <MagnifyingGlassIcon className="w-6 text-[#F4FAFF] opacity-60" />
+            <input
+              className="w-full px-4 py-2 bg-transparent outline-none text-[#F4FAFF] placeholder:text-[#F4FAFF] placeholder:opacity-40"
+              type="text"
+              onChange={(e) => handleSearch(e.target.value)}
+              placeholder="Find by title, tag, code..."
+            />
+          </div>
+        )}
         <div className="border-[#FE6C0B] bg-[#FE6C0B] p-2 rounded-md">
           <Link href="/dashboard/code/create">
             <PlusCircleIcon className="w-6 text-white" />
